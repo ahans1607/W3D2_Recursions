058ba0627c923c102a407f7882572f1335108445
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-01-26 12:45:46 -0600

    first commit

---
+++ problems/05-reverse.js
@@ -0,0 +1,32 @@
+/***********************************************************************
+Write a recursive function reverse(string) that takes in a string and returns
+it reversed.
+
+Examples:
+
+reverse("house"); // "esuoh"
+reverse("dog"); // "god"
+reverse("atom"); // "mota"
+reverse("q"); // "q"
+reverse("id"); // "di"
+reverse(""); // ""
+***********************************************************************/
+
+function reverse (string) {
+  if (string.length === 0){
+    return " ";
+  }
+
+reversy = reverse(string.slice(0, string.length - 1))
+return string[string.length -1] + reversy
+y;
+
+
+}
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = reverse;
+} catch (e) {
+  module.exports = null;
+}
