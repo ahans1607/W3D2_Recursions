7587679ca507e9c1bfddcc5dd286020dc10f0c2f
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-01-30 23:52:25 -0600

    06

--- problems/06-add-to-twelve.js
+++ problems/06-add-to-twelve.js
@@ -8,21 +8,19 @@ Examples:
 
 ***********************************************************************/
 
-// your code here
-function addToTwelve(nums){
-
-  if(nums.length === 0){
-    return false
-  }
-  if (nums.length < 2){
-    return false
+addToTwelve = (array) => {
+  if (array.length <= 0){
+    return false;
   }
 
-    let ifTwelve = (addToTwelve(nums.slice(1)) + nums[0])
-        if (ifTwelve === 12){
+
+  if((array[0] + array[1]) === 12){
     return true
-      }
+    
   }
+
+  return addToTwelve(array.slice(1))
+}
 
 
 
@@ -30,7 +28,29 @@ console.log(addToTwelve([1, 3, 4, 7, 5])); // true
 addToTwelve([1, 3, 4, 7, 6]); // false
 console.log(addToTwelve([1, 11, 4, 7, 6])); // true
 addToTwelve([1, 12, 4, 7, 6]); // false
-console.log(addToTwelve([1])); // false
+console.log(addToTwelve([12])); // false
+
+
+
+
+
+
+// function addToTwelve(nums){
+
+//   if(nums.length === 0){
+//     return false
+//   }
+//   if (nums.length < 2){
+//     return false
+//   }
+
+//     let ifTwelve = (addToTwelve(nums.slice(1)) + nums[0])
+//         if (ifTwelve === 12){
+//     return true
+//       }
+//   }
+
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = addToTwelve;
