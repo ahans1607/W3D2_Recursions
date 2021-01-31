26a76c07ac9491e90535038cbdee4fd3037a3b74
Ananya Hans <ananyahans@Ananyas-iMac.centurylink.net>
2021-01-31 05:02:20 -0600

    12

--- problems/12-is-sorted.js
+++ problems/12-is-sorted.js
@@ -13,32 +13,49 @@ isSorted([5, 4, 3, 2, 1]); // false
 // check if the number before is less that the one after is so True
 // base case: if array[i] > array[i + 1] then false 
 
-let isSorted = (array) => {
+isSorted = (array) => {
 
-if (array[0] > array[0 + 1]){
- 
-  return false
-}
-if (array.length === 2){
-  return true
-}
+  if (array.length === 1){
+    return true
+  }
 
-if (isSorted(array.slice(1))){
+
+  if (array[0] > array[1]){
+    return false
+  }
   
-  return true
- 
-}
-else {
-  return false
-}
+  array.shift()
 
-};
+  return isSorted(array)
+ 
+
+}
 
 console.log(isSorted([1, 2, 3, 4, 5])); // true
 console.log(isSorted([1, 2, 4, 3, 5])); // false
 console.log(isSorted([2, 4, 6, 7, 8])); // true
 console.log(isSorted([5, 4, 3, 2, 1])); // false
 
+// let isSorted = (array) => {
+
+//   if (array[0] > array[0 + 1]){
+   
+//     return false
+//   }
+//   if (array.length === 2){
+//     return true
+//   }
+  
+//   if (isSorted(array.slice(1))){
+    
+//     return true
+   
+//   }
+//   else {
+//     return false
+//   }
+  
+//   };
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = isSorted;
